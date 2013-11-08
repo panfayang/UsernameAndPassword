855c9a5641052c72349ce0e74fe469daaf8e019e
pan <panfayang@gmail.com>
2013-11-08 00:07:23 -0500

    first

---
+++ src/com/example/usernameandpassword/DialogSignin.java
@@ -0,0 +1,41 @@
+package com.example.usernameandpassword;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+
+public class DialogSignin extends DialogFragment {
+	
+	@Override
+	public Dialog onCreateDialog(Bundle saveInstanceState){
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		// Get the layout inflater 
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+		
+		// Inflate and set the layout for the dialog
+		// Pass null as the parent view because its going in the dialog layout
+		builder.setView(inflater.inflate(R.layout.activity_dialog_signin,null))
+		// Add action buttons
+		
+			.setPositiveButton(R.string.signin, new DialogInterface.OnClickListener(){
+				@Override
+				public void onClick(DialogInterface dialog, int id){
+					//sign in the user
+				}
+			})
+			.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int id){
+					DialogSignin.this.getDialog().cancel();
+				}
+					
+			});
+		
+		return builder.create();
+	}
+	
+	
+
+}
